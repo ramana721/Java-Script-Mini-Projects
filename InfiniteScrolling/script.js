575d9e780c292071b8a47bf65a390f2c027ebef7
Tyagi <2357386@cognizant.com>
2025-05-01 17:45:30 +0530

    Movie Search Added

---
+++ InfiniteScrolling/script.js
@@ -0,0 +1,17 @@
+const apikey = "_DDIVJSgdK-GI1wA3aHOtxC9YTt8tCY6-4jMk7guznY";
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apikey}&count=${count}`;
+
+function displayPhotos() {
+	// Display images on UI
+}
+
+
+async function getPhotos() 
+    //fetch images from Unslaph API
+}
+
+window.addEventListener("scroll", () =>{
+	// Triggers the getPhotos() function, if scroll is at the end
+});
+
+getPhotos()
